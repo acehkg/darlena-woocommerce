f39d6ebd148a8348aedb6575c69f3ed915d1dcf1
Andrew <andrewembury@me.com>
2021-10-09 10:01:19 +0800

    Added apollo client and basic auth functions
    added apollo client and graphql
    created necessary basic auth functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+//apollo client and auth functions
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../lib/apolloClient';
+import { AuthProvider } from '../hooks/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default MyApp;
