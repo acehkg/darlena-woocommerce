b4711c52c983a52901f0d57b12333722e505b3a4
Andrew <andrewembury@me.com>
2021-10-10 22:15:43 +0800

    Customer Context
    Built out the initial version of the customer context (typed) and implemented in in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,23 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 
-//apollo client and auth functions
+//apollo client ,auth and customer functions
 import { ApolloProvider } from '@apollo/client';
 import { client } from '../lib/apolloClient';
 import { AuthProvider } from '../hooks/useAuth';
+import { CustomerProvider } from '../hooks/useCustomer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
-        <ChakraProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
+        <CustomerProvider>
+          <ChakraProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </CustomerProvider>
       </AuthProvider>
     </ApolloProvider>
   );
