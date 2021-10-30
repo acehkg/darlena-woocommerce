42cd7be6fb7705a3e9690aaa1003e788940f1dcb
Andrew <andrewembury@me.com>
2021-10-29 21:38:12 -0700

    Fixed zIndex on Logo

--- components/common/Navbar/Logo.jsx
+++ components/common/Navbar/Logo.jsx
@@ -2,7 +2,7 @@ import { NavLink } from '../ui/NavLink';
 import { Image } from '@chakra-ui/react';
 
 export const Logo = ({ ...rest }) => (
-  <NavLink href={'/'}>
+  <NavLink href={'/'} zIndex='1'>
     <Image src='/logo-svg.svg' alt='Company Logo' {...rest} />
   </NavLink>
 );
