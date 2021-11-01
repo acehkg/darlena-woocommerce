95952412156396e64296aae2a7a5e73e77bb77c7
Andrew <andrewembury@me.com>
2021-10-31 23:16:40 -0800

    removed buttons from produc card as per client request

--- components/product/ProductCard/ProductCard.jsx
+++ components/product/ProductCard/ProductCard.jsx
@@ -1,14 +1,13 @@
-import { Box, Skeleton, Stack, Text, useDisclosure } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Skeleton, Stack, Text } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { FavouriteButton } from './FavoriteButton';
 import { useProduct } from '../../../hooks/useProduct';
 import { PriceTag } from './PriceTag';
-import { ProductButtonGroup } from './ProductButtonGroup';
 import ProductCardImage from '../../common/Image/ProductCardImage';
-import DetailsModal from '../DetailsModal/DetailsModal';
 
 export const ProductCard = ({ product }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const [isLoading, setIsLoading] = useState(true);
   const { data, loading, error } = useProduct(product);
   useEffect(() => {
@@ -29,37 +28,40 @@ export const ProductCard = ({ product }) => {
         borderRadius='xl'
       >
         <Box position='relative' className='group' h='100%' w='100%'>
-          <ProductCardImage
-            image={product.image}
-            ratio={3 / 4}
-            objectFit='cover'
-          />
-
+          <Link href={`/${product.id}`} passHref>
+            <ChakraLink
+              transition='.5s'
+              _hover={{
+                filter: 'brightness(80%)',
+              }}
+            >
+              <ProductCardImage
+                image={product.image}
+                ratio={3 / 4}
+                objectFit='cover'
+              />
+            </ChakraLink>
+          </Link>
           <FavouriteButton
             position='absolute'
             top='3'
             right='3'
             aria-label={`Add ${product.name} to your favourites`}
           />
-          <Box
-            opacity='0'
-            transition='opacity 0.1s'
-            _groupHover={{
-              opacity: 1,
-            }}
-            position='absolute'
-            bottom='3'
-            left='3'
-            right='3'
-            bg='brandPink.100'
-            borderRadius='xl'
-            padding='1.5'
-          >
-            <ProductButtonGroup id={product?.id} onOpen={onOpen} />
-          </Box>
         </Box>
         <Stack direction='row' px='1rem' justifyContent='space-between'>
-          <Text fontWeight='medium'>{product.name}</Text>
+          <Link href={`/${product.id}`} passHref>
+            <ChakraLink
+              transition='.5s'
+              _hover={{
+                textDecoration: 'none',
+                fontWeight: 'bold',
+                transform: 'scale(1.05)',
+              }}
+            >
+              <Text fontWeight='medium'>{product.name}</Text>
+            </ChakraLink>
+          </Link>
 
           {isLoading ? (
             <Skeleton height='1rem' width='100%' />
@@ -79,9 +81,6 @@ export const ProductCard = ({ product }) => {
           )}
         </Stack>
       </Stack>
-      {loading ? null : (
-        <DetailsModal isOpen={isOpen} onClose={onClose} product={product} />
-      )}
     </>
   );
 };
