d9c1bc155715e8d9e821eeae23a6f5236273233a
Andrew <andrewembury@me.com>
2021-11-08 08:04:18 +0800

    NavLink component fixed next/link import

--- components/common/ui/NavLink.jsx
+++ components/common/ui/NavLink.jsx
@@ -1,9 +1,9 @@
-import { Link as NextLink } from 'next/link';
+import Link from 'next/link';
 import { Link as ChakraLink } from '@chakra-ui/react';
 
 export const NavLink = ({ href, children, ...rest }) => {
   return (
-    <ChakraLink as={NextLink} href={href} {...rest}>
+    <ChakraLink as={Link} href={href} {...rest}>
       {children}
     </ChakraLink>
   );
